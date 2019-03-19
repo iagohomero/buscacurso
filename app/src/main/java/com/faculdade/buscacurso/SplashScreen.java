2a0554866f9c5e09aa4ad461dd0bf066288befcf
Lucas de Assis Siqueira <lucasasiqueira@outlook.com>
2019-03-18 21:53:16 -0300

    implementação SplashScreen e LoginActivity
    implementação SplashScreen e LoginActivity

--- app/src/main/java/com/faculdade/buscacurso/SplashScreen.java
+++ app/src/main/java/com/faculdade/buscacurso/SplashScreen.java
@@ -1,16 +1,61 @@
 package com.faculdade.buscacurso;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity
 {
+    Thread splashTread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        StartAnimations();
+    }
+
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        ConstraintLayout c = (ConstraintLayout) findViewById(R.id.cons_lay);
+        c.clearAnimation();
+        c.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.imgLogo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3000) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashScreen.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashScreen.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
 
     }
 }
