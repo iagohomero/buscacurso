02cb72b6a3ef857ee9d1790d01a3001711f635cd
Lucas de Assis Siqueira - MacBook <lucasasiqueira@outlook.com>
2019-03-20 19:38:09 -0300

    commit chamar CadastroCorporativo.class
    commit chamar CadastroCorporativo.class

--- app/src/main/java/com/faculdade/buscacurso/LoginActivity.java
+++ app/src/main/java/com/faculdade/buscacurso/LoginActivity.java
@@ -1,13 +1,32 @@
 package com.faculdade.buscacurso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btCadastrar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btCadastrar = findViewById(R.id.btCadastrar);
+        btCadastrar.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CadastroCorporativo.class);
+                startActivity(intent);
+                finish();
+                return false;
+            }
+        });
+        
+
     }
 }
