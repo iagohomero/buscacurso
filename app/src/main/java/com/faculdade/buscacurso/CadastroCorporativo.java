22f10f8fb37b70899820d37c0317f285cc03e79b
Lucas de Assis Siqueira - MacBook <lucasasiqueira@outlook.com>
2019-04-06 18:05:39 -0300

    implantação do firebase no projeto e exemplos de como cadastrar na classe CadastroCOrporativo.java
    implantação do firebase no projeto e exemplos de como cadastrar na classe CadastroCOrporativo.java

--- app/src/main/java/com/faculdade/buscacurso/CadastroCorporativo.java
+++ app/src/main/java/com/faculdade/buscacurso/CadastroCorporativo.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
->>>>>>> cbaa73cf4342016a1bc6267253aead8b40ba348c
+
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
