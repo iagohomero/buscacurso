9136281e9e255e27429ae7ec064050c295bfc153
Lucas de Assis Siqueira <lucasasiqueira@outlook.com>
2019-04-21 13:26:44 -0300

    Implementação do login, retirada do cadastro corporativo da tela de login e inserção da HomeActivity

--- app/src/main/java/com/faculdade/buscacurso/LoginActivity.java
+++ app/src/main/java/com/faculdade/buscacurso/LoginActivity.java
@@ -2,6 +2,7 @@ package com.faculdade.buscacurso;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,6 +30,9 @@ public class LoginActivity extends AppCompatActivity
     private Button btCadastrarUsuario;
     private Button btCadastroEmpresa;
     private Button btLogin;
+
+    private FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
 
     private EditText edtEmail;
     private EditText edtSenha;
@@ -100,23 +114,83 @@ public class LoginActivity extends AppCompatActivity
 
     private void RealizarLogin()
     {
-        //método responsável por realizar o login
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-        builder.setTitle("Função não implementada")
-                .setMessage("Função não realizada até o momento..")
-                .setCancelable(false)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener()
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(Email, Senha).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if(task.isSuccessful())
                 {
-                    public void onClick(DialogInterface dialog, int id)
-                    {
-                        dialog.cancel();
-                        return;
-                    }
-                });
-        AlertDialog alert = builder.create();
-        alert.show();
-        return;
+                    databaseReference = FirebaseDatabase.getInstance().getReference().child("Usuarios/"+firebaseAuth.getCurrentUser().getUid());
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                        {
+                            if(dataSnapshot.exists())
+                            {
+                                Intent intent = new Intent(LoginActivity.this, Home.class);
+                                startActivity(intent);
+                            }
+                            else
+                            {
+                                DatabaseReference databaseReferenceCorporativo = FirebaseDatabase.getInstance().getReference().child("UsuariosCorporativos/"+firebaseAuth.getCurrentUser().getUid());
+                                databaseReferenceCorporativo.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                                    {
+                                        if(dataSnapshot.exists())
+                                        {
+                                            Intent intent = new Intent(LoginActivity.this, Home.class);
+                                            startActivity(intent);
+                                        }
+                                        else
+                                        {
+                                            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                            builder.setTitle("Usuário não cadastrado")
+                                                    .setCancelable(false)
+                                                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(DialogInterface dialog, int which) {
+                                                            dialog.dismiss();
+                                                        }
+                                                    });
+                                            AlertDialog alert = builder.create();
+                                            alert.show();
+                                        }
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+                else
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setTitle("Email ou senha inválidos")
+                            .setCancelable(false)
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+            }
+        });
     }
 
     private boolean isValidEmail(String email)
