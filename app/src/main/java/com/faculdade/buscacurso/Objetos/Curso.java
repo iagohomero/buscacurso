4172cec1a355b3c73802ebfc36c60c951af43583
pomerwe <pomerwe@gmail.com>
2019-05-29 14:33:05 -0300

    Classe Curso criada

---
+++ app/src/main/java/com/faculdade/buscacurso/Objetos/Curso.java
@@ -0,0 +1,159 @@
+package com.faculdade.buscacurso.Objetos;
+
+public class Curso {
+
+    private String Id;
+    private String Nome;
+    private String Requisito;
+    private String Carga_Horaria;
+    private String Data_Inicio;
+    private String Data_Fim;
+    private String Data_Inicio_Inscricao;
+    private String Data_Fim_Inscricao;
+    private String nome_Estabelecimento;
+    private String Codigo_Estabelecimento;
+    private String Limite_Alunos;
+    private String Tipo_curso;
+    private String Nota_Mec;
+    private String Preco;
+    private String Bolsa;
+    private String Materias;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getRequisito() {
+        return Requisito;
+    }
+
+    public void setRequisito(String requisito) {
+        Requisito = requisito;
+    }
+
+    public String getCarga_Horaria() {
+        return Carga_Horaria;
+    }
+
+    public void setCarga_Horaria(String carga_Horaria) {
+        Carga_Horaria = carga_Horaria;
+    }
+
+    public String getData_Inicio() {
+        return Data_Inicio;
+    }
+
+    public void setData_Inicio(String data_Inicio) {
+        Data_Inicio = data_Inicio;
+    }
+
+    public String getData_Fim() {
+        return Data_Fim;
+    }
+
+    public void setData_Fim(String data_Fim) {
+        Data_Fim = data_Fim;
+    }
+
+    public String getData_Inicio_Inscricao() {
+        return Data_Inicio_Inscricao;
+    }
+
+    public void setData_Inicio_Inscricao(String data_Inicio_Inscricao) {
+        Data_Inicio_Inscricao = data_Inicio_Inscricao;
+    }
+
+    public String getData_Fim_Inscricao() {
+        return Data_Fim_Inscricao;
+    }
+
+    public void setData_Fim_Inscricao(String data_Fim_Inscricao) {
+        Data_Fim_Inscricao = data_Fim_Inscricao;
+    }
+
+    public String getNome_Estabelecimento() {
+        return nome_Estabelecimento;
+    }
+
+    public void setNome_Estabelecimento(String nome_Estabelecimento) {
+        this.nome_Estabelecimento = nome_Estabelecimento;
+    }
+
+    public String getCodigo_Estabelecimento() {
+        return Codigo_Estabelecimento;
+    }
+
+    public void setCodigo_Estabelecimento(String codigo_Estabelecimento) {
+        Codigo_Estabelecimento = codigo_Estabelecimento;
+    }
+
+    public String getLimite_Alunos() {
+        return Limite_Alunos;
+    }
+
+    public void setLimite_Alunos(String limite_Alunos) {
+        Limite_Alunos = limite_Alunos;
+    }
+
+    public String getTipo_curso() {
+        return Tipo_curso;
+    }
+
+    public void setTipo_curso(String tipo_curso) {
+        Tipo_curso = tipo_curso;
+    }
+
+    public String getNota_Mec() {
+        return Nota_Mec;
+    }
+
+    public void setNota_Mec(String nota_Mec) {
+        Nota_Mec = nota_Mec;
+    }
+
+    public String getPreco() {
+        return Preco;
+    }
+
+    public void setPreco(String preco) {
+        Preco = preco;
+    }
+
+    public String getBolsa() {
+        return Bolsa;
+    }
+
+    public void setBolsa(String bolsa) {
+        Bolsa = bolsa;
+    }
+
+    public String getMaterias() {
+        return Materias;
+    }
+
+    public void setMaterias(String materias) {
+        Materias = materias;
+    }
+
+    public String getArea_Curso() {
+        return Area_Curso;
+    }
+
+    public void setArea_Curso(String area_Curso) {
+        Area_Curso = area_Curso;
+    }
+
+    private String Area_Curso;
+}
