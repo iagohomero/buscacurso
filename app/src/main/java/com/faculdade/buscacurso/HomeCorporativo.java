8c9ff7e1023d88d75065412b7a6502630308a159
Estevão Alves <pomerwe@gmail.com>
2019-05-31 14:48:56 -0300

    Botões da home page direcionam para casdastrar curso

--- app/src/main/java/com/faculdade/buscacurso/HomeCorporativo.java
+++ app/src/main/java/com/faculdade/buscacurso/HomeCorporativo.java
@@ -1,15 +1,31 @@
 package com.faculdade.buscacurso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class HomeCorporativo extends AppCompatActivity
 {
 
+    private LinearLayout btCadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_corporativo);
+
+        btCadastro = findViewById(R.id.btCadastro);
+        btCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pushCursoCadastro();
+            }
+        });
+    }
+    public void pushCursoCadastro(){
+        Intent intent = new Intent(getApplicationContext(),CadastroCurso.class);
+        startActivity(intent);
     }
 }
